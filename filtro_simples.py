0c63bb6e5edb5d7cfe05d0bb4acdac24a8d50732
andrecastrom06 <alcms@cesar.school>
2026-02-05 16:30:15 -0300

    fix: Definição de metodologia e inicio dos scripts

---
+++ filtro_simples.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from extrair_performance import medir_performance, salvar_resultado
+
+# ================= PANDAS =================
+def filtrar_participantes_pandas(caminho_csv):
+    df = pd.read_csv(caminho_csv, sep=';', encoding='latin1')
+    df["TP_FAIXA_ETARIA"] = pd.to_numeric(df["TP_FAIXA_ETARIA"], errors="coerce")
+    return df[df["TP_FAIXA_ETARIA"] > 15]
+
+def filtrar_resultados_pandas(caminho_csv):
+    df = pd.read_csv(caminho_csv, sep=';', encoding='latin1')
+    df["NU_NOTA_REDACAO"] = pd.to_numeric(df["NU_NOTA_REDACAO"], errors="coerce")
+    return df[df["NU_NOTA_REDACAO"] > 800]
+
+# ================= SPARK =================
+def criar_sessao():
+    return (
+        SparkSession.builder
+        .appName("Comparativo Pandas vs Spark")
+        .master("local[*]")
+        .config("spark.driver.host", "127.0.0.1")
+        .config("spark.driver.bindAddress", "127.0.0.1")
+        .config("spark.ui.showConsoleProgress", "false")
+        .getOrCreate()
+    )
+
+def filtrar_participantes_spark(caminho_csv):
+    spark = criar_sessao()
+
+    df = (
+        spark.read
+        .option("header", True)
+        .option("sep", ";")
+        .option("encoding", "iso-8859-1")
+        .csv(caminho_csv)
+    )
+
+    df = df.withColumn("TP_FAIXA_ETARIA", col("TP_FAIXA_ETARIA").cast("int"))
+    return df.filter(col("TP_FAIXA_ETARIA") > 15), spark
+
+
+def filtrar_resultados_spark(caminho_csv):
+    spark = criar_sessao()
+
+    df = (
+        spark.read
+        .option("header", True)
+        .option("sep", ";")
+        .option("encoding", "iso-8859-1")
+        .csv(caminho_csv)
+    )
+
+    df = df.withColumn("NU_NOTA_REDACAO", col("NU_NOTA_REDACAO").cast("int"))
+    return df.filter(col("NU_NOTA_REDACAO") > 800), spark
+
+
+# ================= MAIN =================
+if __name__ == "__main__":
+
+    testes = [
+        ("Dados_ENEM/PARTICIPANTES_2024.csv", filtrar_participantes_pandas, "pandas"),
+        ("Dados_ENEM/RESULTADOS_2024.csv", filtrar_resultados_pandas, "pandas"),
+        ("Dados_ENEM/PARTICIPANTES_2024.csv", filtrar_participantes_spark, "spark"),
+        ("Dados_ENEM/RESULTADOS_2024.csv", filtrar_resultados_spark, "spark"),
+    ]
+
+    for arquivo, func, tipo in testes:
+        stats = medir_performance(func, arquivo)
+        salvar_resultado("Resultados/filtro_simples.csv", arquivo, tipo, stats)
+
+    print("\nBenchmark finalizado. Resultados salvos.")
